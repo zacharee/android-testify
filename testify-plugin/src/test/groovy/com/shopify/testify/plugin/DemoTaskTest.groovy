e24c9b3ce90081b5cead6ca8120420fbf7051a6b
Daniel Jette <daniel.jette@shopify.com>
2016-06-05 20:27:03 -0400

    Plugin tests

---
+++ testify-plugin/src/test/groovy/com/shopify/testify/plugin/DemoTaskTest.groovy
@@ -0,0 +1,16 @@
+package com.shopify.testify.plugin
+
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Test
+
+import static org.junit.Assert.assertTrue
+
+class DemoTaskTest {
+    @Test
+    public void should_be_able_to_add_task_to_project() {
+        Project project = ProjectBuilder.builder().build()
+        def task = project.task('demo', type: DemoTask)
+        assertTrue(task instanceof DemoTask)
+    }
+}
