24f9edafc9e1fcd8200e759e596b0e16c3ab05d0
Daniel Jette <daniel.jette@shopify.com>
2016-06-09 12:08:24 -0400

    Groovy

---
+++ plugin/src/main/groovy/BlogPlugin.groovy
@@ -0,0 +1,14 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class BlogPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        def showDevicesTask = target.tasks.create("showDevices") << {
+            def adbExe = target.android.getAdbExe().toString()
+            println "${adbExe} devices".execute().text
+        }
+        showDevicesTask.group = "blogplugin"
+        showDevicesTask.description = "Runs adb devices command"
+    }
+}
