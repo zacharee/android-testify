b9e7ba2346ccc0072f71f16ccedaf67fe3335671
Daniel Jette <daniel.jette@shopify.com>
2017-01-28 17:47:20 -0500

    Initial version

--- sample/app/src/androidTest/java/com/shopify/testifysample/BasicTests.java
+++ sample/app/src/androidTest/java/com/shopify/testifysample/BasicTests.java
@@ -2,7 +2,6 @@ package com.shopify.testifysample;
 
 import com.shopify.testify.ScreenshotTest;
 import com.shopify.testify.ScreenshotTestCase;
-import com.shopify.testify.TestHarnessActivity;
 
 /**
  * Created by danieljette on 2016-03-03.
@@ -11,10 +10,10 @@ import com.shopify.testify.TestHarnessActivity;
 public class BasicTests extends ScreenshotTestCase<TestHarnessActivity> {
 
     public BasicTests() {
-        super(TestHarnessActivity.class, com.shopify.testify.R.id.component_placeholder);
+        super(TestHarnessActivity.class, R.id.component_placeholder);
     }
 
     public void testBootstrap() throws Exception {
-        new ScreenshotTest(this, com.shopify.testify.R.layout.test_bootstrap).assertSame();
+        new ScreenshotTest(this, R.layout.test_bootstrap).assertSame();
     }
 }
---
+++ sample/app/src/androidTest/java/com/shopify/testifysample/TestHarnessActivity.java
@@ -0,0 +1,14 @@
+package com.shopify.testifysample;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TestHarnessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_component_test);
+    }
+}
