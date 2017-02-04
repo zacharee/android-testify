fe09be97a4784f39de366791fa69b2daa590988f
Daniel Jette <daniel.jette@shopify.com>
2017-02-04 00:45:28 -0500

    Clean up sample code

--- sample/app/src/androidTest/java/com/shopify/testifysample/BasicTests.java
+++ sample/app/src/androidTest/java/com/shopify/testifysample/BasicTests.java
@@ -1,12 +1,31 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Shopify Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.shopify.testifysample;
 
 import com.shopify.testify.ScreenshotTest;
 import com.shopify.testify.ScreenshotTestCase;
 
-/**
- * Created by danieljette on 2016-03-03.
- * Copyright © 2016 Shopify. All rights reserved.
- */
 public class BasicTests extends ScreenshotTestCase<TestHarnessActivity> {
 
     public BasicTests() {
@@ -15,5 +34,9 @@ public class BasicTests extends ScreenshotTestCase<TestHarnessActivity> {
 
     public void testBootstrap() throws Exception {
         new ScreenshotTest(this, R.layout.test_bootstrap).assertSame();
+    }
+
+    public void testMainActivity() throws Exception {
+        new ScreenshotTest(this, R.layout.activity_main).assertSame();
     }
 }
