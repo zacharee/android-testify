d8ecbc192f40570473beff5c53a727e43bad35d3
Daniel Jette <daniel.jette@shopify.com>
2017-03-06 14:02:26 -0500

    Updated screenshots

--- sample/app/src/androidTest/java/com/shopify/testifysample/SampleJUnit4Tests.java
+++ sample/app/src/androidTest/java/com/shopify/testifysample/SampleJUnit4Tests.java
@@ -2,12 +2,17 @@ package com.shopify.testifysample;
 
 import android.support.test.runner.AndroidJUnit4;
 
+import com.shopify.testify.BaseScreenshotTest;
 import com.shopify.testify.ScreenshotTestRule;
 import com.shopify.testify.annotation.TestifyLayout;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 public class SampleJUnit4Tests {
@@ -23,5 +28,16 @@ public class SampleJUnit4Tests {
     @Test
     public void ruleUsingSetter() throws Exception {
         screenshotTestRule.setLayoutId(R.layout.activity_main);
+    }
+
+    @Test
+    @TestifyLayout(layoutId = R.layout.test_sample)
+    public void usingEspresso() throws Exception {
+        screenshotTestRule.setEspressoActions(new BaseScreenshotTest.EspressoActions() {
+            @Override
+            public void performEspressoActions() {
+                onView(withId(R.id.checkBox)).perform(click());
+            }
+        });
     }
 }
--- testify/src/main/java/com/shopify/testify/ScreenshotTestRule.java
+++ testify/src/main/java/com/shopify/testify/ScreenshotTestRule.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
+import android.view.View;
 
 import com.shopify.testify.annotation.TestifyLayout;
 
@@ -16,9 +17,10 @@ import org.junit.runners.model.Statement;
 @SuppressWarnings({"unused", "WeakerAccess"})
 public class ScreenshotTestRule<T extends Activity> extends ActivityTestRule<T> implements TestRule {
 
+    @LayoutRes private int layoutId;
     private String testName;
     private Throwable throwable;
-    @LayoutRes private int layoutId;
+    private BaseScreenshotTest.EspressoActions espressoActions;
 
     public ScreenshotTestRule(Class<T> activityClass) {
         super(activityClass);
@@ -26,26 +28,28 @@ public class ScreenshotTestRule<T extends Activity> extends ActivityTestRule<T> 
 
     @Override
     public Statement apply(Statement base, Description description) {
+        throwable = null;
+        espressoActions = null;
         testName = description.getTestClass().getSimpleName() + "_" + description.getMethodName();
-        TestifyLayout testifyLayout = description.getAnnotation(TestifyLayout.class);
-        if (testifyLayout != null) {
-            layoutId = testifyLayout.layoutId();
-        }
+        final TestifyLayout testifyLayout = description.getAnnotation(TestifyLayout.class);
+        layoutId = (testifyLayout != null) ? testifyLayout.layoutId() : View.NO_ID;
         return new ScreenshotStatement(super.apply(new BaseInterceptorStatement(base), description));
     }
 
-    public void setLayoutId(@LayoutRes int layoutId) {
+    public ScreenshotTestRule setLayoutId(@LayoutRes int layoutId) {
         this.layoutId = layoutId;
+        return this;
     }
 
-    @Override
-    protected void beforeActivityLaunched() {
-        throwable = null;
+    public ScreenshotTestRule setEspressoActions(BaseScreenshotTest.EspressoActions espressoActions) {
+        this.espressoActions = espressoActions;
+        return this;
     }
 
     private void afterTestStatement() {
         ScreenshotTest screenshotTest = new ScreenshotTest(layoutId);
         try {
+            screenshotTest.setEspressoActions(espressoActions);
             screenshotTest.assertSame();
             int a = 0;
         } catch (Throwable throwable) {
