3c9ea800ce73190d94bd8dccdcaacd63d63a4437
Daniel Jette <daniel.jette@shopify.com>
2018-12-06 15:15:54 -0500

    Initial version of sample app

---
+++ Sample/app/src/androidTest/java/com/shopify/testify/sample/ExampleActivityTest.kt
@@ -0,0 +1,14 @@
+package com.shopify.testify.sample
+
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ExampleActivityTest {
+
+    @Test
+    fun mainActivityDefault() {
+
+    }
+}
