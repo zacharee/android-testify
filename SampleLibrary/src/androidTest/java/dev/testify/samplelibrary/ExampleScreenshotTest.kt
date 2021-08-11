0f82142675455593455018a9f75d35f234f5d8cf
Daniel Jette <daniel.jette@neofinancial.com>
2021-08-10 20:44:54 -0400

    Add library project on 4.2

---
+++ SampleLibrary/src/androidTest/java/dev/testify/samplelibrary/ExampleScreenshotTest.kt
@@ -0,0 +1,17 @@
+package dev.testify.samplelibrary
+
+import com.shopify.testify.ScreenshotRule
+import com.shopify.testify.annotation.ScreenshotInstrumentation
+import org.junit.Rule
+import org.junit.Test
+
+class ExampleScreenshotTest {
+
+    @get:Rule val screenshotTestRule = ScreenshotRule(MainActivity::class.java)
+
+    @ScreenshotInstrumentation
+    @Test
+    fun useAppContext() {
+        screenshotTestRule.assertSame()
+    }
+}
